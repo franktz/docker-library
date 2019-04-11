8fd5143979423a915b11398675072c4ce9bf92a6
FrankTown <franktz2003@gmail.com>
2019-04-11 15:23:26 +0800

    fully deploy k8s v1.14.1

--- shell/k8s.gcr.io-v1.14.1.sh
+++ shell/k8s.gcr.io-v1.14.1.sh
@@ -7,7 +7,8 @@ images=(
     pause:3.1
     etcd:3.3.10
     coredns:1.3.1
-    flannel:v0.11.0-amd64
+
+    kubernetes-dashboard-amd64:v1.10.1
 )
 
 for key in ${images[@]} ; do
@@ -15,3 +16,8 @@ for key in ${images[@]} ; do
     docker tag registry.cn-shenzhen.aliyuncs.com/franktown/$key k8s.gcr.io/$key
     docker rmi registry.cn-shenzhen.aliyuncs.com/franktown/$key
 done
+
+
+docker pull registry.cn-shenzhen.aliyuncs.com/franktown/flannel:v0.11.0-amd64
+docker tag registry.cn-shenzhen.aliyuncs.com/franktown/flannel:v0.11.0-amd64 quay.io/coreos/flannel:v0.11.0-amd64
+docker rmi registry.cn-shenzhen.aliyuncs.com/franktown/flannel:v0.11.0-amd64
